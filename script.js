a2fc7704cca2f56aa1ebbdb4f6344ed01068199f
lcnde <176673026+lcnde@users.noreply.github.com>
2021-06-01 09:32:47 +0200

    update

--- script.js
+++ script.js
@@ -8,9 +8,39 @@ function Book(title, author, npages, read) {
   this.read = read;
 };
 
+
+
+
+
+var table = document.getElementById("table");
+
+
 function addBookToLibrary() {
+  let libtitle = document.getElementById("title").value;
+  let libauthor = document.getElementById("author").value;
+  let libpages = document.getElementById("npages").value;
+  let libread = document.getElementById("radio").value;
   
-};
+  let newBook = new Book(libtitle, libauthor, libpages, libread);
+  myLibrary.push(newBook);
+
+  
+  
+
+ // myLibrary.forEach(function(item) {
+    console.log("ok")
+    var row = table.insertRow(1);
+    var titlecell = row.insertCell(0);
+    var authorcell = row.insertCell(1);
+    var pagecell = row.insertCell(2);
+    var readcell = row.insertCell(3);
+    titlecell.innerHTML = `${libtitle}`;
+    authorcell.innerHTML = `${libauthor}`;
+    pagecell.innerHTML = `${libpages}`;
+    readcell.innerHTML = `${libread}`;
+  };
+  
+  
 
 
 
