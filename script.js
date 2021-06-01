19de85f406d8e3f7755f4f27d8857a7d2fd81fbb
lcnde <176673026+lcnde@users.noreply.github.com>
2021-06-01 06:55:39 +0200

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, npages, read) {
+  // The constructor
+  this.title = title;
+  this.author = author;
+  this.npages = npages;
+  this.read = read;
+};
+
+function addBookToLibrary() {
+  
+};
+
+
+
